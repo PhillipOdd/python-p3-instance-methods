d42ac802872b6df9018e899ca7eebec6b4868201
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-09-06 09:24:23 +0300

    Add files via upload

---
+++ lib/person.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class Person:
+    # Class body goes here
+
+    #Instance method definition
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
+
+guido = Person()
+guido = Person()
+
+
+
+guido.talk()
+guido.walk()      
